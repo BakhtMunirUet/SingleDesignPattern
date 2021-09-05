2f91847914dd70efd1acdb0e6212cc3fe6a1a718
BakhtMunirUet <bakhtmuniruet@gmail.com>
2021-09-05 10:44:31 -0700

    Add project files.

---
+++ SingleDesignPattern/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SingleDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonClass.Instance.Add();
+            SingletonClass.Instance.Update();
+            SingletonClass.Instance.Delete();
+        }
+    }
+
+    public class SingletonClass
+    {
+        public static SingletonClass Instance { get; set; } = new SingletonClass();
+
+        protected internal SingletonClass()
+        {
+        }
+
+        public virtual void Add() 
+        {
+            Console.WriteLine("Add method called......");
+        }
+
+        public virtual void Update()
+        {
+            Console.WriteLine("Update method called.....");
+        }
+
+
+        public virtual void Delete()
+        {
+            Console.WriteLine("Delete method called......");
+        }
+
+
+
+    }
+
+}
